e5439a9b26a6cc2c47d218ff1af6be8081f72068
SnoWFLakE0s <inventor1234567890@gmail.com>
2021-12-04 05:20:40 -0500

    some extra messing with the timing and overlap

--- Sound Mod/Assets/script.cs
+++ Sound Mod/Assets/script.cs
@@ -7,6 +7,7 @@ public class script : MonoBehaviour
     //Variables
     private AudioSource[] AudioSources;
     public AudioClip MyClip;
+    private bool HasInstantiated;
     
     // Start is called before the first frame update
     void Start()
@@ -17,14 +18,31 @@ public class script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ServiceProvider.Instance.PlayerAircraft.Controls.FireWeapons == true) {
+            DetectCannonFiring();
+        }
+        if (ServiceProvider.Instance.PlayerAircraft.Controls.FireWeapons == false) {
+            HasInstantiated = false;
+        }
     }
 
-    private void GetAudioSources(){
+    private void GetAudioSources() {
         AudioSources = FindObjectsOfType<AudioSource>();
-        foreach (AudioSource AS in AudioSources){
-            if (AS.name == "LaunchSound"){
+        foreach (AudioSource AS in AudioSources) {
+            if (AS.name == "LaunchSound") {
             AS.clip = MyClip;
+            }
+        }
+    }
+
+    private void DetectCannonFiring() {
+        AudioSource LaunchSound = GameObject.Find("LaunchSound").GetComponent<AudioSource>();
+        if (LaunchSound.isPlaying == true) {
+            if (HasInstantiated == false) {
+                GameObject NewAudioSource = Instantiate(GameObject.Find("LaunchSound"));
+                NewAudioSource.GetComponent<AudioSource>().Play();
+
+                HasInstantiated = true;
             }
         }
     }
